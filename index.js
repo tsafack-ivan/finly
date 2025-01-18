cd53821bde211c01df84a3cceceada0661521685
tsafack-ivan <ivanmarctsafack@gmail.com>
2025-01-18 10:05:25 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
+
 const express = require('express');
 const morgan = require('morgan');
+const session = require('express-session');
+
 require('dotenv').config();
 require('./libs/dbConnect');
+
+const userRouter = require('./routes/user.route');
+const dashboardRouter = require('./routes/dashboard.route');
+
 const app = express();
-app.set('views', './views');
-app.set('view engine', 'ejs');
+
+@@ -13,20 +15,18 @@ app.set('view engine', 'ejs');
+
 app.use(morgan('dev'));
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => {
-   res.render('index', { message: 'Hello From Node.js' });
+  res.render('index', { message: 'Hello From Node.js' });
 });
+
+app.use('/users', userRouter);
+
 app.get('/contact', (req, res) => {
-   res.render('index', { message: 'The Contact Page' });
+  res.render('index', { message: 'The Contact Page' });
 });
+
 app.get('/about', (req, res) => {
-   res.render('index', { message: 'The About Page' });
+  res.render('index', { message: 'The About Page' });
 });
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: process.env.AUTH_SECRET,
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+app.use('/', userRouter);
+app.use('/dashboard', dashboardRouter);
+
 app.get('*', (req, res) => {
-res.status(404).render('index', { message: 'Not Found' });
-});
-const PORT = 3000;
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
-});
+  res.status(404).render('index', { message: 'Not Found' });
+
+  const flash = require('connect-flash');
+// ...
+app.use(flash());
